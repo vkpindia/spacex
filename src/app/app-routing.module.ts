5214cc7b47667b7bd16d8e8f207fabf6e2964407
vkpindia <infovkpg@gmail.com>
2020-09-28 11:52:28 +0530

    git deploy

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'spaceX',
+    pathMatch: 'full'
+  },
+  {
+    path: 'spaceX',
+    loadChildren: () => import('./modules/space-x/space-x.module').then(m => m.SpaceXModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
