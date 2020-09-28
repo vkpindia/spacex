0499701a6d0e0e07566ad4d2d21c21b6bc4dd6ce
vkpindia <infovkpg@gmail.com>
2020-09-28 12:38:24 +0530

    server setup

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-app.use(express.static(__dirname+'/dist'));
-app.listen(process.env.PORT||8080);
-app.get('*/', function(req, res){
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
-})
-
-console.log('App listening');
+app.use(express.static(__dirname + '/dist/spacex'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/spacex/index.html'));});
+app.listen(process.env.PORT || 8080);
